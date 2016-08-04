2273a600a9c912337ca4af17f83fd58106b920fa
Administrator <Administrator@krtyasoftwares.net>
2016-08-04 17:40:37 +0530

    Project structure

---
+++ src/AddressMapSystem/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using AddressMapSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressMapSystem.Services
+{
+    public interface IUserService
+    {
+        void AddUser(User User);
+    }
+}
